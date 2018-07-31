36b78380cfde986e9b663e9ebb4763596be45177
马静怡 <472382917@qq.com>
2018-07-31 10:50:18 -0400

    promise-weather

--- app-promise.js
+++ app-promise.js
@@ -16,16 +16,34 @@ const argv = yargs
 console.log('!!!!!',argv);
 
 var encodeAddress = encodeURIComponent(argv.address);
-geocodeURL = `http://maps.googleap.com/maps/api/geocode/json?address=${encodeAddress}`;
+geocodeURL = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`;
 
 
 axios.get(geocodeURL)
 .then((res)=>{
-    console.log(JSON.stringify(res.data, undefined, 2))
-})
-.catch((e)=>{
-    if(e.code ==='ENOTFOUND'){
-        console.log("UNABLE!!!");
+    if(res.data.status==='ZERO_RESULTS'){
+        throw new Error('Unable to find that address!!!')
     }
 
+    var lat = res.data.results[0].geometry.location.lat;
+    var lng = res.data.results[0].geometry.location.lng;
+    var address = res.data.results[0].formatted_address;
+    var weatherUrl = `https://api.darksky.net/forecast/2baefed1fc393dd8598e0f8c2db15317/${lat},${lng}`;
+    console.log(JSON.stringify(address));
+    return axios.get(weatherUrl)
 })
+    .then((res)=>{
+        var temperature = res.data.currently.temperature;
+        var apparentTemperature = res.data.currently.apparentTemperature;
+        console.log(`it is currently ${temperature}, it feels like ${apparentTemperature}`)
+    })
+.catch((e)=>{
+    if(e.code ==='ENOTFOUND'){
+        console.log("unable to connect to API servers!!!");
+    }else{
+        console.log(e.message)
+    }
+    }
+
+)
+
